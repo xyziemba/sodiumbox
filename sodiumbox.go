c39115d9dee2f1164dff54ca4bfd6e90be46d83d
Xy Ziemba <xy.ziemba@gmail.com>
2017-11-14 18:57:13 -0800

    Initial commit

---
+++ sodiumbox.go
@@ -0,0 +1,96 @@
+package sodiumbox
+
+import (
+	"crypto/rand"
+	"io"
+
+	"github.com/pkg/errors"
+
+	"golang.org/x/crypto/blake2b"
+	naclbox "golang.org/x/crypto/nacl/box"
+)
+
+// NonceSize is the number of bytes in a nonce
+const NonceSize = 24
+
+// KeySize is the number of bytes in a key
+const KeySize = 32
+
+// Overhead is `len(boxedMsg) - len(originalMessage)`
+const Overhead = naclbox.Overhead + KeySize
+
+// GenerateKey creates a new keypair. It simply wraps the
+// underlying NaCl generator.
+func GenerateKey(rand io.Reader) (publicKey, privateKey *[32]byte, err error) {
+	return naclbox.GenerateKey(rand)
+}
+
+// Seal encrypts a message such that it can be read with the peer's keypair
+func Seal(msg []byte, peersPublicKey *[KeySize]byte) (box []byte, err error) {
+	ephemeralPubkey, ephemeralPrivkey, err := naclbox.GenerateKey(rand.Reader)
+	if err != nil {
+		return nil, errors.Wrap(err, "sodiumbox: unable to generate keys")
+	}
+
+	nonce, err := genNonce(ephemeralPubkey, peersPublicKey)
+	if err != nil {
+		return nil, errors.Wrap(err, "sodiumbox: unable to generate nonce")
+	}
+
+	overhead := naclbox.Overhead + len(ephemeralPubkey)
+	box = make([]byte, 0, overhead+len(msg))
+	box = append(box, ephemeralPubkey[:]...)
+	box = naclbox.Seal(box, msg, nonce, peersPublicKey, ephemeralPrivkey)
+	return
+}
+
+// Open authenticates and decrypts a box such that it can be read
+func Open(box []byte, pubKey, privKey *[32]byte) (msg []byte, err error) {
+	if len(box) < KeySize {
+		return nil, errors.New("sodiumbox: box contains less data than a pubkey")
+	}
+
+	var peerPubkey [KeySize]byte
+	copy(peerPubkey[:], box[:KeySize])
+	boxedMsg := box[KeySize:]
+
+	nonce, err := genNonce(&peerPubkey, pubKey)
+
+	msg, ok := naclbox.Open(nil, boxedMsg, nonce, &peerPubkey, privKey)
+	if !ok {
+		return nil, errors.New("sodiumbox: unable to open box")
+	}
+	return
+}
+
+// genNonce creates a nonce from the two pubkeys in this transaction.
+// This result must not be used more than once with the same pair of
+// public keys.
+func genNonce(ephemeralPubkey, pubKey *[KeySize]byte) (*[NonceSize]byte, error) {
+	nonce := &[NonceSize]byte{}
+
+	nonceHasher, err := blake2b.NewXOF(NonceSize, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = nonceHasher.Write(ephemeralPubkey[:])
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = nonceHasher.Write(pubKey[:])
+	if err != nil {
+		return nil, err
+	}
+
+	n, err := nonceHasher.Read(nonce[:])
+	if err != nil {
+		return nil, err
+	}
+	if n != NonceSize {
+		return nil, err
+	}
+
+	return nonce, nil
+}
---
+++ sodiumbox_test.go
@@ -0,0 +1,42 @@
+package sodiumbox_test
+
+import (
+	"crypto/rand"
+	"testing"
+
+	"github.com/xyziemba/sodiumbox"
+
+	"github.com/stretchr/testify/assert"
+	naclbox "golang.org/x/crypto/nacl/box"
+)
+
+var roundtripTests = []struct {
+	name    string
+	content []byte
+}{
+	{"Nil", nil},
+	{"Single", []byte("A")},
+	{"String", []byte("This is s string!")},
+	{"Unicode", []byte("🤠")},
+}
+
+func TestRoundtrip(t *testing.T) {
+	pubkey, privkey, err := naclbox.GenerateKey(rand.Reader)
+	assert.NoError(t, err)
+
+	for _, test := range roundtripTests {
+		t.Run(test.name, func(t *testing.T) {
+			msg := []byte(test.content)
+			_, err = rand.Read(msg)
+			assert.NoError(t, err)
+
+			box, err := sodiumbox.Seal(msg, pubkey)
+			assert.NoError(t, err)
+			assert.Equal(t, len(msg)+sodiumbox.Overhead, len(box))
+
+			actMsg, err := sodiumbox.Open(box, pubkey, privkey)
+			assert.NoError(t, err)
+			assert.Equal(t, msg, actMsg)
+		})
+	}
+}
